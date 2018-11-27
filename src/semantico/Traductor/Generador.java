f62f33bd2403f66e4fefc2b73dcbd7338b743124
berrocal9470 <32211491+berrocal9470@users.noreply.github.com>
2018-11-26 21:44:29 -0600

    Añadido el cambio de salto según operador

--- src/semantico/Traductor/Generador.java
+++ src/semantico/Traductor/Generador.java
@@ -196,12 +196,15 @@ public class Generador {
 
     public static String EstrucControl_IF(PilaSemantica pPila) // unicamente de la forma [if (a < b)]
     {
-        String traduccion =
-                "cmp (" + pPila.pop_init().getValor() + " " + pPila.pop_init().getValor() + " " +
-                pPila.pop_init().getValor() + ")\n" +
-                "jz ";
+        String operando1 = pPila.pop_init().getValor();
+        String operador = pPila.pop_init().getValor();
+        String operando2 = pPila.pop_init().getValor();
 
-        etiqSiguiente = nuevaEtiq(); // Parece redundancia de asignación pero es necesario que 'etiqAnterior' y 'etiqSiguiente' contengan el valor
+        String traduccion =
+                "cmp (" + operando1 + " " + operador + " " + operando2 + ")\n" +
+                getSalto(operador) + " ";
+
+        etiqSiguiente = nuevaEtiq();
         traduccion += etiqSiguiente + "\n";  //else o end
         pilaEnd.add(etiqSiguiente);
 
@@ -233,8 +236,12 @@ public class Generador {
 
         String traduccion = "startWhile_" + etiqAnterior + ":\n";
 
-        traduccion += "cmp (" + pPila.pop_init().getValor() + " " + pPila.pop_init().getValor() + " " + pPila.pop_init().getValor() + ")\n";
-        traduccion += "jg " + "exitWhile_" + etiqAnterior + "\n";
+        String operando1 = pPila.pop_init().getValor();
+        String operador = pPila.pop_init().getValor();
+        String operando2 = pPila.pop_init().getValor();
+
+        traduccion += "cmp (" + operando1 + " " + operador + " " + operando2 + ")\n";
+        traduccion += getSalto(operador) + " exitWhile_" + etiqAnterior + "\n";
         // contenido del while
 
         return traduccion;
@@ -286,6 +293,18 @@ public class Generador {
                 System.err.println("Error en la generación de código");
         }
 
+    }
+
+    private static String getSalto(String operador){
+        switch(operador){
+            case "<":   return "jge";
+            case "<=":  return "jg";
+            case ">":   return "jle";
+            case ">=":  return "jl";
+            case "<>":  return "je";
+            case "=":   return "jne";
+            default:    return "jz";
+        }
     }
 
 	public static String nuevaTemp()
